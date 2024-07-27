b7fad11270996589b9df38a9f680216177e71b49
Eduardo-Saravia <carlos.saravia.analytics@gmail.com>
2024-07-26 23:52:20 -0500

    Update glue-spotify-project.py

--- glue-jobs/glue-spotify-project.py
+++ glue-jobs/glue-spotify-project.py
@@ -5,32 +5,79 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
+# Parse the arguments passed to the script
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+# Initialize Spark and Glue contexts
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
+
+# Initialize the Glue job
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Script generated for node artist
-artist_node1722034642076 = glueContext.create_dynamic_frame.from_options(format_options={"quoteChar": "\"", "withHeader": True, "separator": ","}, connection_type="s3", format="csv", connection_options={"paths": ["s3://project-spotify-datewhitdata/staging/artists.csv"], "recurse": True}, transformation_ctx="artist_node1722034642076")
+# Extract data from the artists CSV file in S3
+artist_node = glueContext.create_dynamic_frame.from_options(
+    format_options={"quoteChar": "\"", "withHeader": True, "separator": ","},
+    connection_type="s3",
+    format="csv",
+    connection_options={"paths": ["s3://project-spotify-datewhitdata/staging/artists.csv"], "recurse": True},
+    transformation_ctx="artist_node"
+)
 
-# Script generated for node album
-album_node1722034642773 = glueContext.create_dynamic_frame.from_options(format_options={"quoteChar": "\"", "withHeader": True, "separator": ","}, connection_type="s3", format="csv", connection_options={"paths": ["s3://project-spotify-datewhitdata/staging/albums.csv"], "recurse": True}, transformation_ctx="album_node1722034642773")
+# Extract data from the albums CSV file in S3
+album_node = glueContext.create_dynamic_frame.from_options(
+    format_options={"quoteChar": "\"", "withHeader": True, "separator": ","},
+    connection_type="s3",
+    format="csv",
+    connection_options={"paths": ["s3://project-spotify-datewhitdata/staging/albums.csv"], "recurse": True},
+    transformation_ctx="album_node"
+)
 
-# Script generated for node tracks
-tracks_node1722034643438 = glueContext.create_dynamic_frame.from_options(format_options={"quoteChar": "\"", "withHeader": True, "separator": ","}, connection_type="s3", format="csv", connection_options={"paths": ["s3://project-spotify-datewhitdata/staging/track.csv"], "recurse": True}, transformation_ctx="tracks_node1722034643438")
+# Extract data from the tracks CSV file in S3
+tracks_node = glueContext.create_dynamic_frame.from_options(
+    format_options={"quoteChar": "\"", "withHeader": True, "separator": ","},
+    connection_type="s3",
+    format="csv",
+    connection_options={"paths": ["s3://project-spotify-datewhitdata/staging/track.csv"], "recurse": True},
+    transformation_ctx="tracks_node"
+)
 
-# Script generated for node Join Album & Artist
-JoinAlbumArtist_node1722034836808 = Join.apply(frame1=album_node1722034642773, frame2=artist_node1722034642076, keys1=["artist_id"], keys2=["id"], transformation_ctx="JoinAlbumArtist_node1722034836808")
+# Join albums and artists data
+join_album_artist = Join.apply(
+    frame1=album_node,
+    frame2=artist_node,
+    keys1=["artist_id"],
+    keys2=["id"],
+    transformation_ctx="join_album_artist"
+)
 
-# Script generated for node Join with tracks
-Joinwithtracks_node1722035013519 = Join.apply(frame1=tracks_node1722034643438, frame2=JoinAlbumArtist_node1722034836808, keys1=["track_id"], keys2=["track_id"], transformation_ctx="Joinwithtracks_node1722035013519")
+# Join with tracks data
+join_with_tracks = Join.apply(
+    frame1=tracks_node,
+    frame2=join_album_artist,
+    keys1=["track_id"],
+    keys2=["track_id"],
+    transformation_ctx="join_with_tracks"
+)
 
-# Script generated for node Drop Fields
-DropFields_node1722035052452 = DropFields.apply(frame=Joinwithtracks_node1722035013519, paths=["`.track_id`", "id"], transformation_ctx="DropFields_node1722035052452")
+# Drop unnecessary fields
+drop_fields = DropFields.apply(
+    frame=join_with_tracks,
+    paths=["track_id", "id"],
+    transformation_ctx="drop_fields"
+)
 
-# Script generated for node Destination
-Destination_node1722035112736 = glueContext.write_dynamic_frame.from_options(frame=DropFields_node1722035052452, connection_type="s3", format="glueparquet", connection_options={"path": "s3://project-spotify-datewhitdata/datawarehouse/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="Destination_node1722035112736")
+# Write the transformed data back to S3 in Parquet format
+glueContext.write_dynamic_frame.from_options(
+    frame=drop_fields,
+    connection_type="s3",
+    format="glueparquet",
+    connection_options={"path": "s3://project-spotify-datewhitdata/datawarehouse/", "partitionKeys": []},
+    format_options={"compression": "snappy"},
+    transformation_ctx="destination"
+)
 
+# Commit the Glue job
 job.commit()
